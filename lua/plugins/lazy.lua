4ad130d91c530d1a11c9b73931598ce310a753f6
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-06 16:11:12 +0100

    feat: Added lualine configuration

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -215,4 +215,49 @@ require("lazy").setup({
 			vim.g.undotree_RelativeTimestamp = 1
 		end
 	},
+	{
+		'nvim-lualine/lualine.nvim',
+		init = function()
+			require('lualine').setup {
+				options = {
+					icons_enabled = false,
+					theme = 'powerline',
+					component_separators = { left = '', right = ''},
+					section_separators = { left = '', right = ''},
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+					ignore_focus = {},
+					always_divide_middle = true,
+					globalstatus = false,
+					refresh = {
+						statusline = 1000,
+						tabline = 1000,
+						winbar = 1000,
+					}
+				},
+				sections = {
+					lualine_a = {'mode'},
+					lualine_b = {'branch', {'diagnostics', colored=true}},
+					lualine_c = {{'filename', path=3}},
+					lualine_x = {'encoding', 'fileformat', 'filetype'},
+					lualine_y = {'progress'},
+					lualine_z = {'location'}
+				},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {'filename'},
+					lualine_x = {'location'},
+					lualine_y = {},
+					lualine_z = {}
+				},
+				tabline = {},
+				winbar = {},
+				inactive_winbar = {},
+				extensions = {}
+			}
+		end
+	},
 })
