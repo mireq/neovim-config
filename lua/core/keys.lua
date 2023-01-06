de88822e3325de28fadeea3628fb6dccc269aa80
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-06 15:52:38 +0100

    feat: Added indent shortcuts

--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -11,6 +11,19 @@ vim.keymap.set('', '<C-k>', 'gk')
 -- Remap home
 vim.keymap.set('', '<Home>', '^')
 vim.keymap.set('i', '<Home>', '<C-O>^', {silent=true})
+
+-- common completion
+vim.keymap.set('i', '<C-Space>', '<C-X><C-O>')
+vim.keymap.set('i', '<Nul>', '<C-X><C-O>')
+
+
+-- Adjust indent
+vim.api.nvim_create_autocmd({"BufEnter", "InsertLeave"}, {
+	callback = function()
+		vim.keymap.set('x', '<Tab>', '>gv')
+		vim.keymap.set('x', '<BS>', '<gv')
+	end
+})
 --
 --
 --
@@ -31,10 +44,6 @@ vim.keymap.set('n', '<F2>', term.toggle)
 vim.keymap.set('t', '<C-w>', '<C-\\><C-n>')
 vim.keymap.set('t', '<C-q>', '<C-\\><C-n>:lua require("term").toggle()<CR>')
 
--- " Save with ctrl+s
+-- Save with ctrl+s
 vim.keymap.set('n', '<C-S>', function() cmd("w") end)
 vim.keymap.set('i', '<C-S>', function() vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', false); cmd('w') end)
-
--- common completion
-vim.keymap.set('i', '<C-Space>', '<C-X><C-O>')
-vim.keymap.set('i', '<Nul>', '<C-X><C-O>')
