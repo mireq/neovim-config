8404849d3043adab3f747f6e8132c6ad28a4d20b
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-06 16:22:27 +0100

    feat: Added gitsigns

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,7 +1,7 @@
 require("lazy").setup({
-	"gpanders/editorconfig.nvim",
+	'gpanders/editorconfig.nvim',
 	{
-		"weilbith/nvim-code-action-menu",
+		'weilbith/nvim-code-action-menu',
 		cmd = 'CodeActionMenu',
 		config = function()
 			vim.g.code_action_menu_show_details = false
@@ -257,6 +257,53 @@ require("lazy").setup({
 				winbar = {},
 				inactive_winbar = {},
 				extensions = {}
+			}
+		end
+	},
+	{
+		'lewis6991/gitsigns.nvim',
+		init = function()
+			require('gitsigns').setup {
+				signs = {
+					add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+					change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+					delete       = { hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+					topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+					changedelete = { hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+					untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+				},
+				signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
+				numhl      = true,  -- Toggle with `:Gitsigns toggle_numhl`
+				linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				watch_gitdir = {
+					interval = 1000,
+					follow_files = true
+				},
+				attach_to_untracked = true,
+				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+				},
+				current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000, -- Disable if file is longer than this (in lines)
+				preview_config = {
+					-- Options passed to nvim_open_win
+					border = 'single',
+					style = 'minimal',
+					relative = 'cursor',
+					row = 0,
+					col = 1
+				},
+				yadm = {
+					enable = false
+				},
 			}
 		end
 	},
