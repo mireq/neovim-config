8b0bc7d4583a3afa878121fffa5347c38f538caa
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-08 14:19:58 +0100

    feat: Added completion mappings

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -161,7 +161,13 @@ require("lazy").setup({
 						side_padding = 0,
 					}
 				},
-				mapping = cmp.mapping.preset.insert(),
+				mapping = cmp.mapping.preset.insert({
+					['<C-j>'] = cmp.mapping.scroll_docs(-4),
+					['<C-k>'] = cmp.mapping.scroll_docs(4),
+					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
+					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				}),
 				formatting = {
 					fields = { "kind", "abbr", "menu" },
 					format = function(entry, vim_item)
