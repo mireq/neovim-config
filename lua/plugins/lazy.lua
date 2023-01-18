64df212dae4dd39bd358d38cb571b470ded7c9bf
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-18 07:23:34 +0100

    feat: Tryuing to combine sign_priority with CurrentLineNR

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -345,7 +345,7 @@ require("lazy").setup({
 					ignore_whitespace = false,
 				},
 				current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-				sign_priority = 6,
+				sign_priority = 0,
 				update_debounce = 100,
 				status_formatter = nil, -- Use default
 				max_file_length = 40000, -- Disable if file is longer than this (in lines)
