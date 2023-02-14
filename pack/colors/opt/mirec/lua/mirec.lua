b367c7bdc5e10c9dcc64009e9a56aff3f1fc25bd
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-02-14 06:43:03 +0100

    fix: Better contrast in pmenu

--- pack/colors/opt/mirec/lua/mirec.lua
+++ pack/colors/opt/mirec/lua/mirec.lua
@@ -45,7 +45,7 @@ local colorscheme = {
 	{"Normal", {ctermfg=252, ctermbg=16}},
 	{"Number", {ctermfg=111}},
 	{"Operator", {ctermfg=153, cterm={bold=true}}},
-	{"Pmenu", {ctermfg=248, ctermbg=235}},
+	{"Pmenu", {ctermfg=252, ctermbg=235}},
 	{"PmenuSel", {ctermfg=16, ctermbg=156}},
 	{"PmenuSbar", {ctermfg=248, ctermbg=237}},
 	{"PmenuThumb", {ctermfg=248, ctermbg=156}},
