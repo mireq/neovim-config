dc4e4a71d9079e990e7f7272e5432d58277aea1d
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-07-23 06:47:53 +0200

    feat: Don't use treesitter on large files

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -594,6 +594,13 @@ require("lazy").setup({
 		opts = {
 			highlight = {
 				enable = true,
+				disable = function(lang, buf)
+					local max_filesize = 1024 * 1024 -- 1 MB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
 			},
 			indent = {
 				enable = false
