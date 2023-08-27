aa3d79db80c559f493cea07128d19cc7bb01cd4f
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-08-27 08:35:44 +0200

    feat: Enumerate availeble snippets

--- scripts/ultisnips_to_luasnip.py
+++ scripts/ultisnips_to_luasnip.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env -S nvim --headless --clean --cmd "pyfile %" --cmd "q"
+#!/usr/bin/env -S nvim --headless -n -c "pyfile %" -c "q"
 # -*- coding: utf-8 -*-
 from pathlib import Path
 import vim
+import sys
 
 
+sys.path.append(str(Path.home().joinpath('.local/share/nvim/lazy/ultisnips/pythonx')))
 
 
 def main():
-	print("ok")
+	from UltiSnips import UltiSnips_Manager
+	print(UltiSnips_Manager._snips("", True))
 
 
 if __name__ == "__main__":
