fccb5e5b068d86592dc7d4259c11e39437fe8fa2
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-08-27 06:57:16 +0200

    Make ultisnips_to_luasnip executable in nvim environment

---
+++ scripts/ultisnips_to_luasnip.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S nvim --headless --clean --cmd "pyfile %" --cmd "q"
+# -*- coding: utf-8 -*-
+from pathlib import Path
+import vim
+
+
+
+
+def main():
+	print("ok")
+
+
+if __name__ == "__main__":
+	main()
