65d838e6cacf276f9ac4cb6a23af6731854763b0
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-10-13 14:42:12 +0200

    feat: Added utility function to build multiline lua strings

--- lua/luasnip_snippets/ultisnips_to_luasnip.py
+++ lua/luasnip_snippets/ultisnips_to_luasnip.py
@@ -105,6 +105,19 @@ def escape_lua_string(text: str) -> str:
 	return f'"{LUA_SPECIAL_CHAR_RX.sub(escape_char, text)}"'
 
 
+def escape_multiline_lua_sting(text: str) -> str:
+	equal_signs = 0
+	start_delimiter = ''
+	end_delimiter = ''
+	while True:
+		start_delimiter = f'[{"="*equal_signs}['
+		end_delimiter = f']{"="*equal_signs}]'
+		if start_delimiter not in text and end_delimiter not in text:
+			break
+		equal_signs += 1
+	return f'{start_delimiter}{text}{end_delimiter}'
+
+
 class OrderedSet(dict):
 	def add(self, value: str):
 		self[value] = len(self)
