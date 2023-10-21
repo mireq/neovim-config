e71e6d912553a0580364d82f6f530965dd5b3c27
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-10-21 09:07:43 +0200

    feat: Show snippet name in error message

--- pack/plugins/opt/luasnip_snippets/lua/luasnip_snippets/ultisnips_to_luasnip.py
+++ pack/plugins/opt/luasnip_snippets/lua/luasnip_snippets/ultisnips_to_luasnip.py
@@ -501,7 +501,7 @@ def main():
 				try:
 					fp.write(f'\ts({{{parsed_snippet.attributes}}}, {{{parsed_snippet.get_code(indent=2)}\n\t}}),\n')
 				except Exception:
-					logger.error("Error in sniipet code:\n%s", parsed_snippet.snippet._value)
+					logger.error("Error in snippet '%s':\n%s", parsed_snippet.snippet.trigger, parsed_snippet.snippet._value)
 					raise
 			else:
 				snippet_choices = []
@@ -509,7 +509,7 @@ def main():
 					try:
 						snippet_choices.append(f'\t\t{{{parsed_snippet.get_code(indent=3, replace_zero_placeholders=True)}\n\t\t}},\n')
 					except Exception:
-						logger.error("Error in sniipet code:\n%s", parsed_snippet.snippet._value)
+						logger.error("Error in snippet '%s':\n%s", parsed_snippet.snippet.trigger, parsed_snippet.snippet._value)
 						raise
 				fp.write(f'\ts({{{parsed_snippet.attributes}}}, c(1, {{\n{"".join(snippet_choices)}\t}})),\n')
 		#fp.write(''.join(snippet_code))
