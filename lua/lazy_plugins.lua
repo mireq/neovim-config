0cf82204ea019678a823d901957ef5dd2a8edfd8
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-04-25 05:36:32 +0200

    feat: Added trailing slashes to command file completions

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -348,7 +348,12 @@ require("lazy").setup({
 			cmp.setup.cmdline(':', {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
-					{ name = 'path' }
+					{
+						name = 'path',
+						option = {
+							trailing_slash = true
+						}
+					}
 				}, {
 					{ name = 'cmdline' }
 				})
