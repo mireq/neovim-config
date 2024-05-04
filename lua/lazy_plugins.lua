145680a241748c4ecba11c2c248c072ab46d278c
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-05-04 04:28:44 +0200

    feat: disabling which-key, don't have configuration yet

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -1057,6 +1057,19 @@ require("lazy").setup({
 			require("large_file").setup()
 		end
 	},
+	--{
+	--	"folke/which-key.nvim",
+	--	event = "VeryLazy",
+	--	init = function()
+	--		vim.o.timeout = true
+	--		vim.o.timeoutlen = 300
+	--	end,
+	--	opts = {
+	--		-- your configuration comes here
+	--		-- or leave it empty to use the default settings
+	--		-- refer to the configuration section below
+	--	}
+	--},
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
