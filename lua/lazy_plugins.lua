8bc0de74c55deb2994eea6d82549ad301ac0f553
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-05-05 05:50:58 +0200

    feat: Installed copilot

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -1071,12 +1071,50 @@ require("lazy").setup({
 	--	}
 	--},
 	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
+		"zbirenbaum/copilot.lua",
+		enabled = enable_ai,
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				panel = {
+					enabled = false,
+					auto_refresh = true,
+				},
+				suggestion = {
+					enabled = false,
+					-- use the built-in keymapping for "accept" (<M-l>)
+					auto_trigger = true,
+					accept = false, -- disable built-in keymapping
+				},
+				filetypes = {
+					python = true,
+					php = true,
+					po = true,
+					html = true,
+					htmldjango = true,
+					["*"] = false,
+				},
+			})
+
+			-- hide copilot suggestions when cmp menu is open
+			-- to prevent odd behavior/garbled up suggestions
+			local cmp_status_ok, cmp = pcall(require, "cmp")
+			if cmp_status_ok then
+				cmp.event:on("menu_opened", function()
+					vim.b.copilot_suggestion_hidden = true
+				end)
+
+				cmp.event:on("menu_closed", function()
+					vim.b.copilot_suggestion_hidden = false
+				end)
+			end
 		end,
+	},
 		opts = {
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
