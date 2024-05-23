61c36666b9970ad85c1e94183007360408e75f33
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-05-23 05:46:16 +0200

    feat: Removed indent-blnakline plugin

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -1112,34 +1112,6 @@ require("lazy").setup({
 	--},
 	'ii14/neorepl.nvim',
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-		config = function()
-			require("ibl").setup({
-				debounce = 1000,
-				indent = {
-					char = "┆",
-					repeat_linebreak = true
-				},
-				scope = {
-					enabled = false,
-					show_start = false,
-					show_end = false,
-				},
-			})
-			local hooks = require "ibl.hooks"
-			hooks.register(
-				hooks.type.WHITESPACE,
-				hooks.builtin.hide_first_space_indent_level
-			)
-			hooks.register(
-				hooks.type.WHITESPACE,
-				hooks.builtin.hide_first_tab_indent_level
-			)
-		end
-	},
-	{
 		"zbirenbaum/copilot.lua",
 		enabled = enable_ai,
 		dependencies = {
