6c8d520416178c8c13fdfe9a0c17951b530274fc
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-06-22 21:05:48 +0200

    feat: Enabled signs for staged areas

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -671,6 +671,7 @@ require("lazy").setup({
 					changedelete = { hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
 					untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
 				},
+				signs_staged_enable = true,
 				signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
 				numhl      = true,  -- Toggle with `:Gitsigns toggle_numhl`
 				linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
