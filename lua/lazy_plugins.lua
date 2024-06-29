ebb6ed17a1f78b0a86e727f7f69f992a248e6ec3
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-06-29 06:20:07 +0200

    feat: Removed old signs settings

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -663,14 +663,6 @@ require("lazy").setup({
 				return
 			end
 			gitsigns.setup {
-				signs = {
-					add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
-					change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-					delete       = { hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-					topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-					changedelete = { hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-					untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
-				},
 				signs_staged_enable = true,
 				signcolumn = false, -- Toggle with `:Gitsigns toggle_signs`
 				numhl      = true,  -- Toggle with `:Gitsigns toggle_numhl`
