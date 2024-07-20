a3ee0dc474eb9d766e2eecded2f52005b6682a0a
Miroslav Bendík <miroslav.bendik@gmail.com>
2024-07-20 06:11:56 +0200

    feat: Added window navigation using s shortcut

--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -129,3 +129,9 @@ vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", {silent=true})
 -- Split windows
 vim.keymap.set("n", "sx", ":split<Return>", {silent=true})
 vim.keymap.set("n", "sv", ":vsplit<Return>", {silent=true})
+
+-- move windows
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
