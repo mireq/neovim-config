84387d74bddff01f5b76a39e06e890e7da889075
Miroslav Bendík <miroslav.bendik@gmail.com>
2025-08-20 06:47:56 +0200

    feat: Added single border for hover

--- lua/lazy_plugins.lua
+++ lua/lazy_plugins.lua
@@ -69,7 +69,11 @@ require("lazy").setup({
 				vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 				vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 				vim.keymap.set('n', '<F12>', vim.lsp.buf.definition, bufopts)
-				vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+				vim.keymap.set('n', 'K', function()
+					vim.lsp.buf.hover({
+						border = 'single',
+					})
+				end)
 				vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
 				vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 				vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
