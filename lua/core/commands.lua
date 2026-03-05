82d81ae3ce3dba8972b717f94175c673bdd731c6
Miroslav Bendík <miroslav.bendik@gmail.com>
2026-03-05 05:30:30 +0100

    feat: Bring back LspLog and LspInfo commands

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -128,3 +128,12 @@ end, {})
 vim.api.nvim_create_user_command('CopyLocalFilenameToClipboard', function()
 	vim.fn.setreg('+', vim.fn.expand('%'))
 end, {})
+
+vim.api.nvim_create_user_command('LspLog', function()
+	vim.cmd('edit ' .. vim.lsp.log.get_filename())
+end, {})
+
+
+vim.api.nvim_create_user_command('LspInfo', function()
+	vim.cmd('checkhealth vim.lsp')
+end, {})
